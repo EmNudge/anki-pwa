d8fcc487192c3c4064850adbded258248af80bd4
EmNudge <24513691+EmNudge@users.noreply.github.com>
2026-04-13 07:18:53 +0200

    fix: add aria-label to modal close button
    The close button only had an SVG icon with no accessible name,
    making it invisible to screen readers.

--- src/design-system/components/primitives/Modal.vue
+++ src/design-system/components/primitives/Modal.vue
@@ -65,7 +65,7 @@ function handleContentClick(e: MouseEvent) {
       <div v-if="title || showCloseButton" class="ds-modal__header">
         <h2 v-if="title" class="ds-modal__title">{{ title }}</h2>
         <div v-else />
-        <button v-if="showCloseButton" class="ds-modal__close" @click="emit('close')">
+        <button v-if="showCloseButton" class="ds-modal__close" aria-label="Close" @click="emit('close')">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
             <path
               d="M289.94 256l95-95A24 24 0 0 0 351 127l-95 95l-95-95a24 24 0 0 0-34 34l95 95l-95 95a24 24 0 1 0 34 34l95-95l95 95a24 24 0 0 0 34-34z"
